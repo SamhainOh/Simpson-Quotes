be5e5e4075b1499853d6ef521b94a8358c7412c0
samhain oh <samhainoh@gmail.com>
2019-04-08 11:12:32 +0200

    chalenge-react3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Lamp from './Lamp'
 
-import Quotes from "./Quotes";
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      working : false
+    };
+  }
+
   render() {
+    const test = this.state.working ? logo : 'https://vignette.wikia.nocookie.net/simpsons/images/a/a1/Marge_Simpsons.png/revision/latest?cb=20130817213926&path-prefix=fr'
+    const status = this.state.working ? 'Working':'On break';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={test} className= "AppLogo" alt="logo"/>
           <h1 className ="App-title">Simpsons Quotes</h1>
         </header>
-        <Lamp on />
-        <Lamp />
-        <Quotes/>
 
-       
+        <button 
+        onClick= { () =>{
+          this.setState({working: !this.state.working})
+        }}
+        className = 'Homer'>{status}
+        </button>
       </div>
+      
     );
   }
 }
